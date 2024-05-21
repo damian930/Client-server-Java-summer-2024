b4abe7b7cfb54966b5a02eb9c17f91785602701b
Damian Dragovoz <damiandragovoz@gmail.com>
2024-05-21 22:30:17 +0300

    Made the print better for the invalid packet (lines 34 - 44)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,5 @@
 package org.example;
 
-import java.nio.ByteBuffer;
-import java.util.Arrays;
-
 public class App {
     public static void main( String[] args ) throws Exception {
         Sender s = new Sender();
@@ -37,17 +34,13 @@ public class App {
         {   // invalid packet
             s.create_packet(3, 104, "FNCS was fun this weekend", (byte) 31);
             byte[] corrupted_packet = s.send_packet();
-            corrupted_packet[3] = 13;   // random corruption
+            corrupted_packet[3] = 13;                   // random corruption
             m = r.receive_packet(corrupted_packet);
 
+            System.out.println("Received:");
+            System.out.println("\tCommand type: " + (m != null ? m.getcType() : "null"));
+            System.out.println("\tUser Id: " + (m != null ? m.getbUserId() : "null"));
+            System.out.println("\tText: " + (m != null ? new String(m.getText()) : "null"));
         }
-
-        /*Sender s = new Sender();
-        Receiver r = new Receiver();
-        s.setReceiversKey(r.getPublicKey());
-        s.create_packet(1, 2, "flopper", (byte) 3);
-        System.out.println(Arrays.toString(s.send_packet()));*/
-
-
     }
 }

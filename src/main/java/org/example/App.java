10d657845b4b843cfb8ecbaf569d64114e01c479
Damian Dragovoz <damiandragovoz@gmail.com>
2024-05-31 20:49:31 +0300

    main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,27 +11,6 @@ import java.security.PublicKey;
 
 public class App {
     public static void main(String[] args ) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InterruptedException {
-        /*Decrypter d = new Decrypter();
-        byte[] packet_bytes = create_encrypted_packet("flopper", d.getPublicKey());
-        d.decrypt(packet_bytes);
-        Message m = d.getDecrypted_message();
-
-        System.out.println("\nReceived:");
-        System.out.println("\tCommand type: " + (m != null ? m.getcType() : "null"));
-        System.out.println("\tUser Id: " + (m != null ? m.getbUserId() : "null"));
-        System.out.println("\tText: " + (m != null ? new String(m.getText()) : "null"));
-
-        System.out.println("------------------------------");
-
-        packet_bytes = create_encrypted_packet("dipper pines", d.getPublicKey());
-        d.decrypt(packet_bytes);
-        m = d.getDecrypted_message();
-
-        System.out.println("\nReceived:");
-        System.out.println("\tCommand type: " + (m != null ? m.getcType() : "null"));
-        System.out.println("\tUser Id: " + (m != null ? m.getbUserId() : "null"));
-        System.out.println("\tText: " + (m != null ? new String(m.getText()) + '\n' : "null\n"));*/
-
         Encryptor e = new Encryptor();
         Decrypter d = new Decrypter();
         e.setPublicKey(d.getPublicKey());
